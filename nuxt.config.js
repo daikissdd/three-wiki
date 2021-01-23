37720e234914d7912fc1ae2abdc9a95ce96540c6
daikissdd <daikissdd@gmail.com>
2021-01-23 19:09:44 +0800

    blog

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,84 +1,85 @@
 export default {
-  /*
-   ** Nuxt rendering mode
-   ** See https://nuxtjs.org/api/configuration-mode
-   */
-  mode: 'universal',
-  /*
-   ** Nuxt target
-   ** See https://nuxtjs.org/api/configuration-target
-   */
-  target: 'static',
-  /*
-   ** Headers of the page
-   ** See https://nuxtjs.org/api/configuration-head
-   */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   ** https://nuxtjs.org/guide/plugins
-   */
-  plugins: [],
-  /*
-   ** Auto import components
-   ** See https://nuxtjs.org/api/configuration-components
-   */
-  components: true,
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: ['@nuxt/typescript-build'],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    // Doc: https://github.com/nuxt/content
-    '@nuxt/content',
-  ],
-  /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {},
-  /*
-   ** Content module configuration
-   ** See https://content.nuxtjs.org/configuration
-   */
-  content: {},
-  generate: {
-    routes: function () {
-      const fs = require('fs')
-      const path = require('path')
-      return fs.readdirSync('./content/blog').map((file) => {
-        return {
-          route: `/blog/${path.parse(file).name}`, // Return the slug
-          payload: require(`./content/blog/${file}`),
-        }
-      })
-    },
-  },
-  /*
-   ** Build configuration
-   ** See https://nuxtjs.org/api/configuration-build/
-   */
-  build: {},
-}
+	/*
+	 ** Nuxt rendering mode
+	 ** See https://nuxtjs.org/api/configuration-mode
+	 */
+	mode: 'universal',
+	/*
+	 ** Nuxt target
+	 ** See https://nuxtjs.org/api/configuration-target
+	 */
+	target: 'static',
+	/*
+	 ** Headers of the page
+	 ** See https://nuxtjs.org/api/configuration-head
+	 */
+	head: {
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1',
+			},
+			{
+				hid: 'description',
+				name: 'description',
+				content: process.env.npm_package_description || '',
+			},
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
+	/*
+	 ** Global CSS
+	 */
+	css: [],
+	/*
+	 ** Plugins to load before mounting the App
+	 ** https://nuxtjs.org/guide/plugins
+	 */
+	plugins: [],
+	/*
+	 ** Auto import components
+	 ** See https://nuxtjs.org/api/configuration-components
+	 */
+	components: true,
+	/*
+	 ** Nuxt.js dev-modules
+	 */
+	buildModules: ['@nuxt/typescript-build'],
+	/*
+	 ** Nuxt.js modules
+	 */
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		'@nuxtjs/axios',
+		'@nuxtjs/pwa',
+		// Doc: https://github.com/nuxt/content
+		'@nuxt/content',
+	],
+	/*
+	 ** Axios module configuration
+	 ** See https://axios.nuxtjs.org/options
+	 */
+	axios: {},
+	/*
+	 ** Content module configuration
+	 ** See https://content.nuxtjs.org/configuration
+	 */
+	content: {},
+	generate: {
+		async routes() {
+			const { $content } = require('@nuxt/content');
+			const files = await $content().only(['path']).fetch();
+
+			return files.map((file) =>
+				file.path === '/index' ? '/' : file.path
+			);
+		},
+	},
+	/*
+	 ** Build configuration
+	 ** See https://nuxtjs.org/api/configuration-build/
+	 */
+	build: {},
+};
